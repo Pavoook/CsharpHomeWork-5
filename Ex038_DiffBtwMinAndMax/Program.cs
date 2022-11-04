e8d005c01a746f968dc62c7af8c613620b0639e1
Andrey Khudyakov <avh_2004@mail.ru>
2022-11-04 21:56:50 +0500

    Corrected Exersize №38 Difference Between Minimum and Maximum

--- Ex038_DiffBtwMinAndMax/Program.cs
+++ Ex038_DiffBtwMinAndMax/Program.cs
@@ -2,6 +2,62 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
+int lengthArray = GetSize("Задайте длину массива:");
+double[] collection = new double[lengthArray];
+double maximum = GetBound("Задайте максимальное значение элемента массива:");
+double minimum = GetBound("Задайте минимальное значение элемента массива:");
+if (ValidateBounds(maximum, minimum)) collection = CreateRandomArray(lengthArray, minimum, maximum);
+PrintArray(collection);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна {GetMax(collection) - GetMin(collection)}.");
+
+int GetSize(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine());
+    while (number <= 0)
+    {
+        Console.WriteLine("Неверное значение. Введите целое число больше нуля:");
+        number = int.Parse(Console.ReadLine());
+    }
+    return number;
+}
+
+double GetBound(string msg)
+{
+    Console.WriteLine(msg);
+    double number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+bool ValidateBounds(double max, double min)
+{
+    bool valid = false;
+    while (valid == false)
+    {
+        if (max < min)
+        {
+            Console.WriteLine("Ошибка. Минимум не должен быть больше максимума.");
+            maximum = GetBound("Задайте максимальное значение элемента массива:");
+            max = maximum;
+            minimum = GetBound("Задайте минимальное значение элемента массива:");
+            min = minimum;
+        }
+        else valid = true;
+    }
+    return valid;
+}
+
+double[] CreateRandomArray(int L, double min, double max)
+{
+    double[] array = new double[L];
+    Random r = new Random();
+    for (int i = 0; i < L; i++)
+    {
+        array[i] = r.NextDouble() * (max - min) + min;
+    }
+    return array;
+}
+
 void PrintArray(double[] array)
 {
     Console.Write("[");
@@ -15,7 +71,7 @@ void PrintArray(double[] array)
 double GetMax(double[] array)
 {
     double max = array[0];
-    foreach(double i in array)
+    foreach (double i in array)
     {
         if (i > max) max = i;
     }
@@ -25,13 +81,9 @@ double GetMax(double[] array)
 double GetMin(double[] array)
 {
     double min = array[0];
-    foreach(double i in array)
+    foreach (double i in array)
     {
         if (i < min) min = i;
     }
     return min;
 }
-
-double[] collection = { -102.4, 488.22, -15628.5, 12.5, 1.5, -5698.55, 3.14, 2.71 };
-PrintArray(collection);
-Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна {GetMax(collection)-GetMin(collection)}.");
