a2bafaeb393f40df32df416ce8b00bf0cfbcfbfb
Andrey Khudyakov <avh_2004@mail.ru>
2022-11-04 21:29:35 +0500

    Corrcted Exersize №36 Sum Of Odd Positions

--- Ex036SumOfOddPositions/Program.cs
+++ Ex036SumOfOddPositions/Program.cs
@@ -3,13 +3,59 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-int[] CreateRandomArray(int L)
+int lengthArray = GetSize("Задайте длину массива:");
+int[] collection = new int[lengthArray];
+int maximum = GetBound("Задайте максимальное значение элемента массива:");
+int minimum = GetBound("Задайте минимальное значение элемента массива:");
+if (ValidateBounds(maximum, minimum)) collection = CreateRandomArray(lengthArray, minimum, maximum);
+PrintArray(collection);
+int S = GetSumOfOddPositions(collection);
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях, равна {S}.");
+
+int GetSize(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine());
+    while (number <= 0)
+    {
+        Console.WriteLine("Неверное значение. Введите целое число больше нуля:");
+        number = int.Parse(Console.ReadLine());
+    }
+    return number;
+}
+
+int GetBound(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+bool ValidateBounds(int max, int min)
+{
+    bool valid = false;
+    while (valid == false)
+    {
+        if (max < min)
+        {
+            Console.WriteLine("Ошибка. Минимум не должен быть больше максимума.");
+            maximum = GetBound("Задайте максимальное значение элемента массива:");
+            max = maximum;
+            minimum = GetBound("Задайте минимальное значение элемента массива:");
+            min = minimum;
+        }
+        else valid = true;
+    }
+    return valid;
+}
+
+int[] CreateRandomArray(int L, int min, int max)
 {
     int[] array = new int[L];
     Random r = new Random();
     for (int i = 0; i < L; i++)
     {
-        array[i] = r.Next(-100, 101);
+        array[i] = r.Next(min, max + 1);
     }
     return array;
 }
@@ -35,8 +81,3 @@ int GetSumOfOddPositions(int[] array)
     }
     return s;
 }
-
-int[] collection = CreateRandomArray(10);
-PrintArray(collection);
-int S = GetSumOfOddPositions(collection);
-Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях, равна {S}.");
